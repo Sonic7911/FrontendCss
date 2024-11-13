c30b54fff242f423ce81918daca8919ab62148d6
Sonic7911 <sonicgautam111@gmail.com>
2024-11-13 16:16:00 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div id='P'>
+      <div id='A'>
+        <div id='t-img'></div>
+        <div id='Content'>
+          <div id='title'>Learning</div>
+          <div id='date'>Published 21 Dec 2023</div>
+          <div id='Heading'>HTML and CSS Foundations</div>
+          <div id='description'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, consequatur cum. Totam commodi facere iure
+          </div>
+        </div>
+        {/* Profile section */}
+        <div id='profile'>
+          <div id='profileimg'></div>
+          <div id='profileName'>George Cooper</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Proj1.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+function Proj1() {
+  return(
+    <>
+        <div>
+          
+        </div>
+    
+    </>
+  );
+}
+
+export default Proj1;
