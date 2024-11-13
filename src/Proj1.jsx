ef1278c033d4aa25a4396b5e3e2cca62be1b41ea
Sonic7911 <sonicgautam111@gmail.com>
2024-11-13 19:30:50 +0545

    second card

--- src/Proj1.jsx
+++ src/Proj1.jsx
@@ -1,13 +1,22 @@
-import './App.css';
 import React from 'react';
+import './Proj1css.css';
+import qrImage from './assets/QR.png';
 
 function Proj1() {
-  return(
+  return (
     <>
-        <div>
-          
-        </div>
-    
+    <div id='screen'>
+    <div id='cardHolder'>
+      <div id='qr'>
+        {/* Correct usage of the image source */}
+        <img src={qrImage} alt="QR Code" />
+      </div>
+      <div id='information'>
+          <div id='Heading'>Improve your frontend Skills by building projects</div>
+          <div id='subInfo'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
+      </div>
+    </div>
+    </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Proj1 from './Proj1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Proj1 />
   </StrictMode>,
 )
